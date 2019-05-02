fac0048f9f0b2562eb7857a88a202b98b0c946a8
Elise Feldt <47640837+fancyElise@users.noreply.github.com>
2019-05-02 05:42:20 -0400

    Working proximity sensor (#30)
    * errors, but progress on implementation
    
    * having to make updates to Vector3 and Array
    
    * ProximitySensor is ready

--- VASTTesting/Sensor.h
+++ VASTTesting/Sensor.h
@@ -18,7 +18,7 @@ public:
 	external to the Obstacle in order to update data important to the Obstacle.
 	time		timestamp for the update
 	updateMap	data that changed for this update*/
-	void update(timestamp t, dataMap dataMap);	
+	virtual void update(timestamp t, dataMap dataMap);	
 
 	/* Informs the component that the replication is coming to an end.  This function
 	is called by the EventTree to give the component the opportunity to reset data, or
@@ -37,7 +37,7 @@ public:
 	/* Overridden from VComponent.  Returns a copy of the internal data map.*/
 	virtual dataMap getDataMap();
 
-private:
+protected:
 	
 	string _name;
 
