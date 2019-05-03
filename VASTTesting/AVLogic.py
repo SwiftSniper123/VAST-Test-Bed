30f6db1a6bb107197a3e898e5294a9b30afddd9b
SwiftSniper123 <47580947+SwiftSniper123@users.noreply.github.com>
2019-05-02 22:04:57 -0400

    Two Python Scripts
    One that is the correct following logic and one to guarantee a crash

---
+++ VASTTesting/AVLogic.py
@@ -0,0 +1,31 @@
+import csv
+import os
+import sys
+
+path = sys.path[0]
+
+with open(path + '\\AVInput.csv', 'rt', encoding="utf-8-sig",newline='') as infile:
+    reader = csv.reader(infile, delimiter=',')
+    headers = next(reader)
+    print (headers)
+    #for row in reader:
+    #    print(row)
+
+    row = next(reader)
+    distance = row[0]
+    speed = row[1]
+
+    if distance < 25:
+        targetSpeed = speed
+        duration = 5
+
+
+
+myData = [['TargetVelocity','DecelTime'],[targetSpeed,duration]]
+
+
+with open(path + '\\AVOutput.csv', 'w', encoding="utf-8-sig", newline='') as outfile:
+    writer = csv.writer(outfile)
+    for row in myData:
+        writer.writerow(row)
+
